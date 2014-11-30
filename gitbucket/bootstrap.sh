cb302cfa33512d0d1271213e17f267bde67d6506
geekduck <duck1218@gmail.com>
2014-11-30 22:44:10 +0900

    Add gitbucket.

---
+++ gitbucket/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DOCKER_IMAGE_NAME=geekduck/gitbucket
+DOCKER_TAG=1.0
+DOCKER_CONTAINER_NAME=gitbucket
+
+docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
+
+docker run -d --name $DOCKER_CONTAINER_NAME \
+  -v /etc/localtime:/etc/localtime:ro \
+  -v /data/gitbucket:/root/.gitbucket \
+  -p 8081:8080 -p 29418:29418 \
+  $DOCKER_IMAGE_NAME:$DOCKER_TAG
