b8746a627b92223277e7a06b497671ae114dc45f
geekduck <duck1218@gmail.com>
2014-11-30 21:44:58 +0900

    create docker files.

---
+++ jenkins/bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+DOCKER_IMAGE_NAME=jenkins
+DOCKER_TAG=latest
+DOCKER_CONTAINER_NAME=jenkins
+
+docker run --name $DOCKER_CONTAINER_NAME \
+  -p 8080:8080 \
+  -v /data/jenkins:/var/jenkins_home
+  $DOCKER_IMAGE_NAME:$DOCKER_TAG
---
+++ minidlna/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DOCKER_IMAGE_NAME=geekduck/minidlna
+DOCKER_TAG=1.0
+DOCKER_CONTAINER_NAME=minidlna
+
+docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG
+
+docker run -d --name $DOCKER_CONTAINER_NAME \
+  --net=host \
+  -v /var/data/public/videos:/data \
+  $DOCKER_IMAGE_NAME:$DOCKER_TAG
