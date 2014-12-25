654903c101bb5b10bcf163b6a9d25dd8f1fcf5c7
geekduck <duck1218+github@gmail.com>
2014-12-26 01:19:15 +0900

    Add URL prefix to Jenkins.

--- jenkins/bootstrap.sh
+++ jenkins/bootstrap.sh
@@ -5,6 +5,7 @@ DOCKER_TAG=weekly
 DOCKER_CONTAINER_NAME=jenkins
 
 docker run -d --name $DOCKER_CONTAINER_NAME \
+  -e JENKINS_OPTS="--prefix=/jenkins" \
   -p 8080:8080 \
   -v /etc/localtime:/etc/localtime:ro \
   -v /data/jenkins:/var/jenkins_home \
