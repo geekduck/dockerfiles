a9152d0070ac78bd4db61e0b20af3c3db392c23f
geekduck <duck1218+github@gmail.com>
2014-12-26 02:20:10 +0900

    Modified link.

--- nginx/bootstrap.sh
+++ nginx/bootstrap.sh
@@ -9,5 +9,6 @@ docker run -d --name $DOCKER_CONTAINER_NAME \
   -v /etc/localtime:/etc/localtime:ro \
   -v `pwd`/conf/nginx.conf:/etc/nginx/nginx.conf:ro \
   -v `pwd`/conf/conf.d:/etc/nginx/conf.d:ro \
+  --link jenkins:jenkins \
+  --link gitbucket:gitbucket \
   $DOCKER_IMAGE_NAME:$DOCKER_TAG
- 
