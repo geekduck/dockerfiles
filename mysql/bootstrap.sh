8e6d93a6e9b5f8e13e3dd992e587a2ab71b9bf82
geekduck <duck1218+github@gmail.com>
2015-08-17 21:30:43 +0900

    Add passward setting.

--- mysql/bootstrap.sh
+++ mysql/bootstrap.sh
@@ -5,6 +5,7 @@ DOCKER_TAG=latest
 DOCKER_CONTAINER_NAME=mysql
 
 docker run -d --name $DOCKER_CONTAINER_NAME \
+  -e MYSQL_ROOT_PASSWORD=mysql \
   -p 3306:3306 \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
