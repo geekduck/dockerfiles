5dd5c5bab8f010fb3764b780f7d1a7e6d6a40330
geekduck <duck1218+github@gmail.com>
2015-08-18 21:06:40 +0900

    Change volume directory.

--- gitbucket/bootstrap.sh
+++ gitbucket/bootstrap.sh
@@ -9,6 +9,6 @@ docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
 docker run -d --name $DOCKER_CONTAINER_NAME \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
-  -v /data/gitbucket:/root/.gitbucket \
+  -v `pwd`/data:/root/.gitbucket \
   -p 29418:29418 \
   $DOCKER_IMAGE_NAME:$DOCKER_TAG
