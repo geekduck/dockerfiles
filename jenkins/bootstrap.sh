1b66335a616a5b09d4a6002d56129b00e73ba6c9
geekduck <duck1218+github@gmail.com>
2015-08-18 21:06:09 +0900

    Change volume directory.

--- jenkins/bootstrap.sh
+++ jenkins/bootstrap.sh
@@ -7,7 +7,7 @@ DOCKER_CONTAINER_NAME=jenkins
 docker run -d --name $DOCKER_CONTAINER_NAME \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
-  -v /data/jenkins:/var/jenkins_home \
+  -v `pwd`/data:/var/jenkins_home \
   -e JENKINS_JAVA_OPTIONS="-Dorg.apache.commons.jelly.tags.fmt.timeZone=Asia/Tokyo" \
   $DOCKER_IMAGE_NAME:$DOCKER_TAG \
   --prefix=/jenkins -Dfile.encoding=UTF-8
