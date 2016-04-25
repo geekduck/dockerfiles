d3fa64b61002dec0e3427cc93b76941bd48c6016
geekduck <duck1218+github@gmail.com>
2016-04-25 21:13:41 +0900

    Update samba.

--- samba/bootstrap.sh
+++ samba/bootstrap.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-DOCKER_IMAGE_NAME=svendowideit/samba
+DOCKER_IMAGE_NAME=dperson/samba
 DOCKER_TAG=latest
 DOCKER_CONTAINER_NAME=samba
 
-DOCKER_VOLUME_IMAGE_NAME=busybox
-DOCKER_VOLUME_TAG=latest
-DOCKER_VOLUME_CONTAINER_NAME=samba_volume
-
-docker run -d --name $DOCKER_VOLUME_CONTAINER_NAME \
+docker run -it -d --name $DOCKER_CONTAINER_NAME \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
-  -v /data \
-  $DOCKER_VOLUME_IMAGE_NAME:$DOCKER_VOLUME_TAG
-
-docker run -d --name $DOCKER_CONTAINER_NAME \
-  -v /etc/localtime:/etc/localtime:ro \
-  -v /etc/timezone:/etc/timezone:ro \
-  $DOCKER_IMAGE_NAME:$DOCKER_TAG
-  $DOCKER_VOLUME_CONTAINER_NAME
+  -p 137:137 \
+  -p 139:139 \
+  -p 445:445 \
+  -v /var/data:/mnt \
+  $DOCKER_IMAGE_NAME:$DOCKER_TAG \
+  -u "`whoami`;`whoami`" \
+  -s "public;/mnt/public;no;no;yes" \
+  -s "private;/mnt/private;no;no;no;`whoami`"
