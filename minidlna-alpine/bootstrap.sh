b4515fa1f9b8e7a9c2f82ccefc570a07038992f4
geekduck <duck1218+github@gmail.com>
2016-04-29 10:59:40 +0900

    Update minidlna.

--- minidlna-alpine/bootstrap.sh
+++ minidlna-alpine/bootstrap.sh
@@ -7,8 +7,8 @@ DOCKER_CONTAINER_NAME=minidlna
 docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
 
 docker run -d --name $DOCKER_CONTAINER_NAME \
-  --net=host \
-  -v /etc/localtime:/etc/localtime:ro \
-  -v /etc/timezone:/etc/timezone:ro \
-  -v /var/data/public/videos:/var/lib/minidlna/videos \
-  $DOCKER_IMAGE_NAME:$DOCKER_TAG
+       --net=host \
+       -v /etc/localtime:/etc/localtime:ro \
+       -v /etc/timezone:/etc/timezone:ro \
+       -v "$MEDIA_VIDEO_DIR":/var/lib/minidlna/videos \
+       $DOCKER_IMAGE_NAME:$DOCKER_TAG
