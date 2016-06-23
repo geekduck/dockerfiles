1d266bb1be6e040132bc9786f44c7d70f8c01647
geekduck <duck1218+github@gmail.com>
2016-06-23 22:07:59 +0900

    Rename minidlna.

--- minidlna/bootstrap.sh
+++ minidlna/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 DOCKER_IMAGE_NAME=geekduck/minidlna
-DOCKER_TAG=1.0
+DOCKER_TAG=alpine
 DOCKER_CONTAINER_NAME=minidlna
 
 docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
@@ -10,5 +10,7 @@ docker run -d --name $DOCKER_CONTAINER_NAME \
   --net=host \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
-  -v /var/data/public/videos:/var/lib/minidlna/videos \
+  -v "$MEDIA_VIDEO_DIR":/var/lib/minidlna/videos \
+  -p 8200:8200 \
+  -p 1900:1900/udp \
   $DOCKER_IMAGE_NAME:$DOCKER_TAG
