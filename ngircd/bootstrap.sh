0a868a407587eea699b028a2e0d38e3818c613eb
geekduck <duck1218+github@gmail.com>
2016-06-24 18:31:08 +0900

    Use docker hub.

--- ngircd/bootstrap.sh
+++ ngircd/bootstrap.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 DOCKER_IMAGE_NAME=geekduck/ngirc
-DOCKER_TAG=1.0
+DOCKER_TAG=latest
 DOCKER_CONTAINER_NAME=ngirc
-
-docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
 
 docker run -d --name $DOCKER_CONTAINER_NAME \
   -p 6667:6667 \
