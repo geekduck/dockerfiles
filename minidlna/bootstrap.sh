9ff797ab910f717dc78f8742f3b4e539d8a0bbdb
geekduck <duck1218+github@gmail.com>
2016-06-24 23:18:27 +0900

    Use dockerhub

--- minidlna/bootstrap.sh
+++ minidlna/bootstrap.sh
@@ -1,16 +1,14 @@
 #!/bin/sh
 
 DOCKER_IMAGE_NAME=geekduck/minidlna
-DOCKER_TAG=alpine
+DOCKER_TAG=latest
 DOCKER_CONTAINER_NAME=minidlna
-
-docker build -t $DOCKER_IMAGE_NAME:$DOCKER_TAG .
 
 docker run -d --name $DOCKER_CONTAINER_NAME \
   --net=host \
   -v /etc/localtime:/etc/localtime:ro \
   -v /etc/timezone:/etc/timezone:ro \
-  -v "$MEDIA_VIDEO_DIR":/var/lib/minidlna/videos \
+  -v "$MEDIA_VIDEO_DIR":/opt/Videos \
   -p 8200:8200 \
   -p 1900:1900/udp \
   $DOCKER_IMAGE_NAME:$DOCKER_TAG
